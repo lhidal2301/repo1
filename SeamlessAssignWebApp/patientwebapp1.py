4813a9ebb2e084fe021e52aa1deb0fef40b6fa4e
lhidal2301 <60224926+lhidal2301@users.noreply.github.com>
2020-01-23 21:47:04 -0500

    SeamlessMD

---
+++ SeamlessAssignWebApp/patientwebapp1.py
@@ -0,0 +1,12 @@
+import json
+from flask import Flask
+
+app = Flask(__name__)
+@app.route("/")
+def patientpage():
+    with open("C:/Users/lykam/OneDrive/Documents/SeamlessMD/patient.json") as patientfile:
+        data = json.load(patientfile)
+        return data
+
+if __name__=="__main__":
+    app.run(debug=True, use_reloader=True)
